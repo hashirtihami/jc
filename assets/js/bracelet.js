c40864a97f8c9d138a57fb4ea8a3d397fac98f59
Wahaj Hussain <wahajhssn@gmail.com>
2018-06-08 15:02:44 +0500

    Added locket.js

--- assets/js/bracelet.js
+++ assets/js/bracelet.js
@@ -1,9 +1,10 @@
 $(".simple").on("click",function() {
 	sessionStorage.setItem("productType","simple");
+	console.log(sessionStorage);
 });
 
 $(".heart").on("click",function() {
 	sessionStorage.setItem("productType","heart");
+	console.log(sessionStorage);
 });
 
-console.log(sessionStorage);
---
+++ assets/js/locket.js
@@ -0,0 +1,25 @@
+$(".simple").on("click",function() {
+	sessionStorage.setItem("productType","simple");
+	console.log(sessionStorage);
+});
+
+$(".heart").on("click",function() {
+	sessionStorage.setItem("productType","heart");
+	console.log(sessionStorage);
+});
+
+$(".fancy").on("click",function() {
+	sessionStorage.setItem("productType","fancy");
+	console.log(sessionStorage);
+});
+
+$(".infinity").on("click",function() {
+	sessionStorage.setItem("productType","infinity");
+	console.log(sessionStorage);
+})
+
+$(".pearl").on("click",function() {
+	sessionStorage.setItem("productType","pearl");
+	console.log(sessionStorage);
+})
+
