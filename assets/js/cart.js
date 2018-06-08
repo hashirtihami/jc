31896001bdefd73cc87669a37f1dba6b6c58be21
Wahaj Hussain <wahajhssn@gmail.com>
2018-06-09 02:32:01 +0500

    Broken cart.js

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,10 +1,9 @@
 var cart = JSON.parse(sessionStorage.getItem("cart")); 
 
-console.log(cart);
-
 for (var key in cart) {
     if (cart.hasOwnProperty(key)) {
-        var html = "<div><h1>Cart</h1></div><div><h3>"+ cart[key].product +"</h3><p>"+ cart[key].productType +"</p><p>"+ cart[key].platingType +
+		var html = "";
+        html += "<div><h3>"+ cart[key].product +"</h3><p>"+ cart[key].productType +"</p><p>"+ cart[key].platingType +
 				"</p><p>"+ cart[key].nameType +"</p><p>"+ cart[key].nameOnProduct +"</p><p>"+ cart[key].nameLanguage +"</p><p>price</p></div>";
 
 		$("body").append(html);
