19363102aeba6a7a89dc7f2dbf9ca0cd62020c3c
Wahaj Hussain <wahajhssn@gmail.com>
2018-06-08 15:03:19 +0500

    console.logs

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,20 @@
 $(".bracelet").on("click",function() {
 	sessionStorage.setItem("product","bracelet");
+	console.log(sessionStorage);
 });
 
 $(".locket").on("click",function() {
 	sessionStorage.setItem("product","locket");
+	console.log(sessionStorage);
 });
 
 $(".ring").on("click",function() {
 	sessionStorage.setItem("product","ring");
+	console.log(sessionStorage);
 });
 
 $(".cuff").on("click",function() {
 	sessionStorage.setItem("product","cufflinks");
+	console.log(sessionStorage);
 });
 
-console.log(sessionStorage);
