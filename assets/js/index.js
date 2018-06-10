5a3af3703924fc0f1d7b76b130279f94dac54cd0
Wahaj Hussain <wahajhssn@gmail.com>
2018-06-10 14:15:10 +0500

    Nothing

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,3 +31,11 @@ $(".cuff").on("click",function() {
 	console.log(sessionStorage);
 });
 
+gapi.client.sheets.spreadsheets.values.get({
+  spreadsheetId: spreadsheetId,
+  range: range
+}).then((response) => {
+  var result = response.result;
+  var numRows = result.values ? result.values.length : 0;
+  console.log(`${numRows} rows retrieved.`);
+});
